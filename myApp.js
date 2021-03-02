fd25b351ded723dabefacd1322ef898c58dc1c3f
Badru <badruakfm@gmail.com>
2021-03-02 20:51:02 +0700

    Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -1,65 +1,78 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-let Person;
 
 const Schema = mongoose.Schema;
 const personSchema = new Schema({
-  name : { type: String, required:true },
-  age : Number,
-  favoriteFoods : [String]
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
 })
 
-Person = mongoose.model("Person", personSchema)
+const Person = mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const andi = Person({
+    name: 'Andi',
+    age: 15,
+    favoriteFoods: ['ikan', 'baso', 'mie ayam']
+  })
+  andi.save((err, data) => {
+    if (err) return console.error(err)
+    done(null, data)
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  done(null /*, data*/ );
 };
 
 /** **Well Done !!**
